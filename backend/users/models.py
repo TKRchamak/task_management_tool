8fed3bc8a1e2a69ff7862faecd478c75a11ff412
TKRchamak <tonmoykumarroy6@gmail.com>
2023-05-10 15:41:45 +0600

    feat(user)
    initialized user update func

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
 class User(AbstractUser):
-    username = models.CharField(max_length = 50, unique = True)
-    email = models.EmailField(unique = True)
-    first_name = models.CharField(max_length = 50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone_no = models.CharField(max_length = 15)
+    phone_no = models.CharField(max_length=15)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
--- backend/users/views.py
+++ backend/users/views.py
@@ -8,29 +8,47 @@ from django.contrib.auth import authenticate, login
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
 
 class UserRegistration(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        print("data: ", request.data.get("username"))
         if serializer.is_valid(raise_exception=True):
             serializer.save()
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 class UserLogin(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
 
         user_auth = authenticate(request, username=username, password=password)
-
+        if not user_auth:
+            raise AuthenticationFailed(
+                {"permission denied": "Wrong credentials"}
+            )
         if user_auth:
             login(request, user_auth)
             token, created = Token.objects.get_or_create(user=user_auth)
         context = {
-            "token":token.key
+            "token": token.key
         }
         return Response(context)
+
+
+class UserUpdate(APIView):
+    authtication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    ''' update user data '''
+
+    def patch(self, request):
+        user = request.user
+        print('user update url : ', user.username)
+        return Response("user updated")
